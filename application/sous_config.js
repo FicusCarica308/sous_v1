8130d6e8e73e21a99d1e104bc2874b9c056dfc1f
Manuel Enrique Figueroa <63696144+FicusCarica308@users.noreply.github.com>
2021-06-23 23:39:47 -0400

    Updated URL to URI

--- application/sous_config.js
+++ application/sous_config.js
@@ -1,3 +1,3 @@
 // This file contains some configuartion details that are prviate and cannot be passed to githubex
 module.exports.spoonicularKey = //'SET-API-KEY-HERE'
-module.exports.mongoURL = //'SET-DATABASE-AUTHENTICATION-URL-HERE;
+module.exports.mongoURI = //'SET-DATABASE-AUTHENTICATION-URL-HERE;
